0563ec2fc7a403a513b4211592e8e0f1cff4fd2c
PatrickFRR <patrick_ribeiro@ymail.com>
2021-09-08 11:58:25 -0300

    Criando e executando test pontosCriarTopicoBonusDoDia

--- src/test/java/TesteRegistroPontos.java
+++ src/test/java/TesteRegistroPontos.java
@@ -24,4 +24,16 @@ public class TesteRegistroPontos {
         rp.criarUmTopico(u);
         assertEquals(u.pontos, 25);
     }
+
+    @Test
+    public void pontosCriarTopicoBonusDoDia() {
+        Usuario u = new Usuario();
+        u.nome = "Rezende";
+        CalculadoraBonus cb = new CalculadoraBonus();
+        cb.bonusDoDia = 3;
+        RegistroPontos rp = new RegistroPontos(cb);
+        rp.criarUmTopico(u);
+        assertEquals(u.pontos, 15);
+    }
+    
 }
