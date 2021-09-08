06d5077b1fbf0cec3bbc1e1dedaf5ac5718b60f8
PatrickFRR <patrick_ribeiro@ymail.com>
2021-09-08 11:32:13 -0300

    Conclusão da classe CalculadoraBonus

--- src/main/java/CalculadoraBonus.java
+++ src/main/java/CalculadoraBonus.java
@@ -2,12 +2,12 @@
 
 public class CalculadoraBonus {
 
-    public int bonusDoDia;
+    public int bonusDoDia = 1;
 
     public int bonus(Usuario u){
         int multiplicador = bonusDoDia;
         if(u.vip)
-            multiplicador = 5;
+            multiplicador *= 5;
         return multiplicador;
     }
 }
