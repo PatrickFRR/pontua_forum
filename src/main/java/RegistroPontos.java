39bc48e2165a2e9a654e7ef67b145b2e96eebbe1
PatrickFRR <patrick_ribeiro@ymail.com>
2021-09-08 11:25:22 -0300

    criando classes registro, usuario, calculadora

---
+++ src/main/java/RegistroPontos.java
@@ -0,0 +1,16 @@
+
+
+public class RegistroPontos {
+
+    public void fazerUmComentario(Usuario u) {
+        u.pontos += 3;
+    }
+
+    public void criarUmTopico(Usuario u) {
+        u.pontos += 5;
+    }
+
+    public void darUmLike(Usuario u) {
+        u.pontos += 1;
+    }
+}
